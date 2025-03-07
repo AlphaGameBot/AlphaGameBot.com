513d3f74c3e61ac6af06eb5b573b9497e813bf73
Damien Boisvert <damien@alphagame.dev>
2025-03-07 11:17:25 -0800

    feat: Add tailwindcss building

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./www/*.html",
+    "./www/**/*.html",
+    "./www/**/*.js",
+    "./www/*.js"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
